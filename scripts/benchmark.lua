cde18881d4fd5fb88c5e26edfa78825743f040a7
Anika Jahagirdar <anika.jahagirdar49@gmail.com>
2026-08-04 02:50:59 +0000

    updates to benchmark.lua (correct latency enabling command, prints latency-related stats, clears stats at start of run

--- scripts/benchmark.lua
+++ scripts/benchmark.lua
@@ -4,17 +4,19 @@ require "Pktgen"
 
 printf("Starting trial\n");
 
+pktgen.clr();
 pktgen.reset(0);
 pktgen.stop(0);
---pktgen.latency(0, 'on');
-sleep(2);
+pktgen.latency('0', 'enable');
+pktgen.delay(200);
 
-printf("Starting port\n");
+printf("Starting traffic\n");
 pktgen.start(0);
 sleep(5);
 
 
 prints("portStats", pktgen.portStats('0', 'port'));
+prints("portRates", pktgen.portStats("0", "rate"));
 
 pktgen.stop(0);
 sleep(1);
