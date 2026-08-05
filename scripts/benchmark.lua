ede9355c7bd026c6cc08d97d679fe4379e0618af
Anika Jahagirdar <anika.jahagirdar49@gmail.com>
2026-08-05 10:36:54 +0000

    script to automate 3-minute benchmark test runs

--- scripts/benchmark.lua
+++ scripts/benchmark.lua
@@ -1,27 +1,16 @@
 package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
-
-require "Pktgen"
-
-printf("Starting trial\n");
+require "Pktgen";
 
 pktgen.clr();
-pktgen.reset(0);
-pktgen.stop(0);
-pktgen.latency('0', 'enable');
+pktgen.delay(100);
+
+pktgen.set(0, "rate", 100);
+pktgen.latency(0, "enable");
 pktgen.delay(200);
 
-printf("Starting traffic\n");
+-- printf("starting traffic\n");
 pktgen.start(0);
-sleep(5);
-
-
-prints("portStats", pktgen.portStats('0', 'port'));
-prints("portRates", pktgen.portStats("0", "rate"));
-
+pktgen.sleep(180);
+-- printf("stopping traffic\n");
 pktgen.stop(0);
-sleep(1);
-
 pktgen.quit();
-
-
-
